1fdd7ea04a2669d9bfc71d900072d2e5445a7793
Mensur K <sabatonz@hotmail.com>
2021-08-06 00:32:52 +0200

    Done(Basics)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import "./style.css";
 import "./modules/showWeather";
+import showWeather from "./modules/showWeather";
+
+if (localStorage.getItem("location") != null)
+  showWeather(localStorage.getItem("location"));
+
+document.querySelector("input[type=submit]").addEventListener("click", () => {
+  document
+    .querySelector("input[type=submit]")
+    .setAttribute("style", "transform: scale(0.95)");
+  setTimeout(() => {
+    document
+      .querySelector("input[type=submit]")
+      .setAttribute("style", "transform: scale(1)");
+  }, 75);
+});
--- src/modules/showWeather.js
+++ src/modules/showWeather.js
@@ -8,14 +8,13 @@ let location = document.querySelector(".location-input");
 const desc = document.querySelector(".weather-desc");
 const body = document.querySelector("body");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const reg = /^[\w\s]{2,20}$/;
-
-  if (reg.test(location.value))
-    getWeather(location.value).then((data) => {
+export default function showWeather(locat) {
+  getWeather(locat)
+    .then((data) => {
       let weather = data.weather[0].main;
+
+      console.log(data);
+
       if (weather == "Clouds") {
         desc.innerHTML = `<i class="fas fa-cloud"></i>`;
         body.style.backgroundImage = `url(${cloudy})`;
@@ -26,10 +25,27 @@ form.addEventListener("submit", (e) => {
         desc.innerHTML = `<i class="fas fa-cloud-rain"></i>`;
         body.style.backgroundImage = `url(${rainy})`;
       }
-      desc.innerHTML += `<h2>${location.value.toUpperCase()} - ${data.weather[0].description.toUpperCase()} ${(
+      desc.innerHTML += `<h2>${locat.toUpperCase()} - ${data.weather[0].description.toUpperCase()} ${(
         data.main.temp - 273.15
       )
         .toString()
         .substr(0, 2)}°C<h2>`;
+
+      form.reset();
+    })
+    .catch(() => {
+      desc.innerHTML = `<p>Location not found</p>`;
     });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const reg = /^[\w\s]{2,20}$/;
+
+  if (reg.test(location.value)) {
+    localStorage.setItem("location", location.value);
+
+    showWeather(location.value);
+  }
 });
